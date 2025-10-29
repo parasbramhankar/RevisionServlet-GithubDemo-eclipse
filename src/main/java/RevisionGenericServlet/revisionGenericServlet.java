87cb8f70d528a78a93f404140db270d7c271c9ac
parasbramhankar <parasbramhankar.in@gmail.com>
2025-10-29 18:23:07 +0530

    First Commit

---
+++ src/main/java/RevisionGenericServlet/revisionGenericServlet.java
@@ -0,0 +1,41 @@
+package RevisionGenericServlet;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class revisionGenericServlet
+ */
+@WebServlet("/test")
+public class revisionGenericServlet extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+
+   
+    public revisionGenericServlet() { 
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	
+		
+	}
+
+}
